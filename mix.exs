3f418873b81dc594b8047893d017b29f6b480835
red <red_davies@belk.com>
2016-03-09 09:19:33 -0500

    Added profiling and monitoring tools.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule CBMQWriter.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:sasl, :logger, :yamerl, :cbserverapi2, :gproc],
+    [applications: [:sasl, :logger, :yamerl, :cbserverapi2, :recon],
      mod: {CBMQWriter, []}]
   end
 
@@ -31,7 +31,9 @@ defmodule CBMQWriter.Mixfile do
     [
       {:yamerl, git: "https://github.com/yakaz/yamerl"},
       {:cbserverapi2, git: "https://github.com/redvers/cbserverapi2.git"},
-      {:gproc, "~> 0.5.0"}
+      {:eep, git: "https://github.com/virtan/eep.git"},
+      {:exrm, "~> 1.0"},
+      {:recon, "~> 2.2"}
     ]
   end
 end
