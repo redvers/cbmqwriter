f7b9a7ea006bd49e95b143f6553bbe063b1a4ac0
red <red_davies@belk.com>
2016-03-09 09:32:01 -0500

    Trading higher process-count for lack of ets

--- lib/cbmqwriter/filesrv/worker.ex
+++ lib/cbmqwriter/filesrv/worker.ex
@@ -3,18 +3,14 @@ defmodule CBMQWriter.Filesrv.Worker do
   use GenServer
 
   def recv_event(eventtype, sensorid, timestamp, binary) do
-    case :gproc.lookup_local_name({:fileservworker, sensorid}) do
-      :undefined ->
-        {:ok, pid} = Supervisor.start_child(CBMQWriter.Filesrv.Supervisor, [sensorid])
-        pid
-      pid ->
-        pid
+    sendingpid =
+    case Process.get(sensorid) do
+      nil -> {:ok, pid} = Supervisor.start_child(CBMQWriter.Filesrv.Supervisor, [sensorid])
+             Process.put(sensorid, pid)
+             pid
+      pid when is_pid(pid) -> pid
     end
-
-    lookuppid = :gproc.lookup_local_name({:fileservworker, sensorid})
-    CBMQWriter.incoming_increment(eventtype)
-    GenServer.cast(lookuppid, {:incoming_event, eventtype, sensorid, timestamp, binary})
-    lookuppid
+    GenServer.cast(sendingpid, {:incoming_event, eventtype, sensorid, timestamp, binary})
   end
 
   def start_link(sensorid) do
@@ -24,15 +20,11 @@ defmodule CBMQWriter.Filesrv.Worker do
 
 
   def init(sensorid) do
-    mypid = self
-    case (:gproc.reg_or_locate({:n, :l, {:fileservworker, sensorid}})) do
-      {^mypid, _} -> :erlang.send_after(10000, mypid, {:sync, sensorid})
-                     {:ok, %{}}
-      {pid, _}    -> :ignore
-    end
+    :erlang.send_after(10000, self, {:sync, sensorid})
+    {:ok, %{}}
   end
 
-  def generate_path({:incoming_event, eventtype, sensorid, timestamp, binary}) do
+  def generate_path({:incoming_event, eventtype, sensorid, timestamp, _binary}) do
     {{year,month,day},{hour,min,_}} = round(timestamp / 10000000) + 50522745600 |> :calendar.gregorian_seconds_to_datetime
     (:io_lib.format('data/~B/~4..0B/~2..0B/~2..0B/~2..0B/~1..0B', [sensorid, year,month,day,hour,div(min, 10)]) |> to_string) <> "/#{eventtype}"
   end
@@ -58,20 +50,15 @@ defmodule CBMQWriter.Filesrv.Worker do
   end
 
   def handle_info({:sync, sensorid}, state) do
-    :gproc.goodbye
-
-    send(self, {:disksync, sensorid})
-    {:noreply, state}
-  end
-  def handle_info({:disksync, sensorid}, state) do
+    send(self, {:gc, sensorid})
     Enum.map(state, &(sync_file(sensorid, &1)))
-    {:stop, :normal, %{}}
-  end
 
-  def terminate(:normal, %{}) do
+    {:noreply, %{}}
   end
-  def terminate(:normal, state) do
-    Logger.debug("Potential race condition - please check thy state #{inspect(state)}")
+  def handle_info({:gc, sensorid}, state) do
+    :erlang.garbage_collect
+    :erlang.send_after(10000, self, {:sync, sensorid})
+    {:noreply, state}
   end
 
 end
